70fbe3361c7b3c9e2cee5d7f90af84139c61e5b9
AlbertoVicentePascual <alvicentepasc@gmail.com>
2025-02-19 16:37:45 +0100

    Enseña "Hello World!"

---
+++ src/main/java/com/iescomercio/prueba_git/PRUEBA_GIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.iescomercio.prueba_git;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class PRUEBA_GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
